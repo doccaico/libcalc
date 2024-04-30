d4c59c33c6a6c31573f7e2eaec7ae1d0d72b351c
doccaico <hassy.luna@gmail.com>
2024-04-30 09:08:27 +0900

    up

---
+++ build.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "calc",
+        .target = target,
+        .optimize = optimize,
+    });
+    // lib.addIncludePath(.{ .path = "include" });
+    lib.addCSourceFiles(.{
+        .files = &.{
+            "src/calc.c",
+            // "src/framing.c",
+        },
+        .flags = &.{
+            // "-std=c99",
+        },
+    });
+    lib.linkLibC();
+    lib.installHeadersDirectory(.{ .path = "include/libcalc" }, "calc", .{});
+    b.installArtifact(lib);
+}
